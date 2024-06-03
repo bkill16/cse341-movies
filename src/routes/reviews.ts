679d8164598bcdcb3d64a7d009990880c71aff49
Brenleigh Killpack <kil21008@byui.edu>
2024-06-03 10:37:00 -0600

    added review validation to review routes

--- src/routes/reviews.ts
+++ src/routes/reviews.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import * as reviewsController from "../controllers/reviews";
+import { idValidationRules, reviewValidationRules, validate } from "../validator";
 
 const router = express.Router();
 
 router.get('/reviews', reviewsController.getAllReviews);
-router.get('/reviews/:id', reviewsController.getSingleReview);
-router.post('/reviews', reviewsController.createReview);
-router.put('/reviews/:id', reviewsController.updateReview);
-router.delete('/reviews/:id', reviewsController.deleteReview);
+router.get('/reviews/:id', ...idValidationRules(), validate, reviewsController.getSingleReview);
+router.post('/reviews', ...reviewValidationRules(), validate, reviewsController.createReview);
+router.put('/reviews/:id', ...reviewValidationRules(), validate, reviewsController.updateReview);
+router.delete('/reviews/:id', ...idValidationRules(), validate, reviewsController.deleteReview);
 
 export default router;
